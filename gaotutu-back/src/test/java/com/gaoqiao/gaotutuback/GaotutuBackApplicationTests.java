6373a4d8beb4727d1c5e5468329e6ece2315d5ab
高桥凉介 <1979836005@qq.com>
2025-07-09 14:43:22 +0800

    后端代码初始化

---
+++ gaotutu-back/src/test/java/com/gaoqiao/gaotutuback/GaotutuBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaoqiao.gaotutuback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaotutuBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
